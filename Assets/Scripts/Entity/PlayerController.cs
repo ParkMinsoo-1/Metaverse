acf8c7c1e32ee87a0a8aa9b0f459be7e3f828ceb
ParkMinsoo <harimaho88@gmail.com>
2025-05-02 20:23:26 +0900

    BaseController, PlayerController, Animation setup

---
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : BaseController
+{
+    private Camera camera;
+
+    protected override void Start()
+    {
+        base.Start();
+        camera = Camera.main;
+    }
+    
+    protected override void HandleAction()
+    {
+        
+    }
+    
+    void OnMove(InputValue inputValue)
+    {
+        movementDirection = inputValue.Get<Vector2>();
+        movementDirection = movementDirection.normalized;
+    }
+
+    void OnLook(InputValue inputValue)
+    {
+        Vector2 mousePosition = inputValue.Get<Vector2>();
+        Vector2 worldPos = camera.WorldToScreenPoint(mousePosition);
+        lookDirection = (worldPos - (Vector2)transform.position);
+
+        if(lookDirection.magnitude < 0.9f)
+        {
+            lookDirection = Vector2.zero;
+        }
+        else
+        {
+            lookDirection = lookDirection.normalized;
+        }
+    }
+
+    void OnFire(InputValue inputValue)
+    {
+        //isAttacking = inputValue.isPressed;
+    }
+
+}
